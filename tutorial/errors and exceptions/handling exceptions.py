5236528edfa0748e106faca7af81d80758de426f
gfYaya <2645919729@qq.com>
2016-12-15 14:15:03 +0800

    handling exceptions

---
+++ tutorial/errors and exceptions/handling exceptions.py
@@ -0,0 +1,23 @@
+# code segment 1
+try:
+    raise Exception('spam', 'eggs')
+except Exception as inst:
+    print(type(inst))
+    print(inst.args)
+    print(inst)
+
+    x, y = inst.args
+    print("x=", x)
+    print("u=", y)
+
+
+# code segment 2
+def this_fails():
+    x = 1 / 0
+
+
+try:
+    this_fails()
+except ZeroDivisionError as zd_err:
+    print(zd_err.args)
+    print("Handling run-time error", zd_err)
