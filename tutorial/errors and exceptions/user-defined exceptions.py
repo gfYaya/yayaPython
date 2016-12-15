e7fbeb0acc73840dec54b4046a906dd23d6c3bc9
gfYaya <2645919729@qq.com>
2016-12-15 15:16:20 +0800

    I know little about this py demostation,need to review few days later

---
+++ tutorial/errors and exceptions/user-defined exceptions.py
@@ -0,0 +1,12 @@
+class MyError(Exception):
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return repr(self.value)
+
+
+try:
+    raise MyError(2 * 2)
+except MyError as my_err:
+    print('My exception occurred , value:', my_err.value)
