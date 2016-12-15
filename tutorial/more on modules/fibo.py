36a50735e213bd54cd9d19bf9839bbbc28bfad1c
gfYaya <2645919729@qq.com>
2016-12-15 13:42:43 +0800

    a new python package with few py files about exceptions

---
+++ tutorial/more on modules/fibo.py
@@ -0,0 +1,17 @@
+#fibonacci numbers module
+
+def fib(n):
+    a,b=0,1
+    while b<n:
+        print(b ,end=' ')
+        a,b=b,a+b
+    print()
+
+def fib2(n):
+    result=[]
+    a,b=0,1
+    while b<n:
+        result.append(b)
+        a,b=b,a+b
+    return result
+    
---
+++ tutorial/more on modules/fibo4using.py
@@ -0,0 +1,10 @@
+#import  fibo
+from fibo import fib,fib2
+print(fibo.fib(1000) )
+print(fibo.fib2(100) )
+print(fibo.__name__)
+
+myfib=fibo.fib
+print(myfib(10))
+myfib_2=fibo.fib2
+print(myfib_2(20) )
