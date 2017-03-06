fbd93f88604085447fba32f76c480fa6b9e02af7
Yaya <2645919729@qq.com>
2017-03-06 22:13:47 +0800

    the complement of   iterator.py

--- tutorial/classes/iterators.py
+++ tutorial/classes/iterators.py
@@ -12,9 +12,8 @@ for char in "123":
     print('iter4string', char)
 
 # for line in open("D:\\ftpdown\\pm250\\city_pm250_datail_20170116-0-50.txt", 'tr'): #错误信息显示的是异步展示
-for line in open("D:\\ftpdown\\pm250\\city_pm250_datail_20170116-0-50.txt", 'tr', encoding='utf-8'):
-    # line=line.decode('gbk')
-    print(line, end="")
+#for line in open("D:\\ftpdown\\pm250\\city_pm250_datail_20170116-0-50.txt", 'tr', encoding='utf-8'):
+   #print(line, end="")
 
 s = "abc"
 it = iter(s)
@@ -30,3 +29,20 @@ except StopIteration:
 # an iterator of py created by myself with the demonstration
 class Reverse:
     """Iterator for looping over a sequence backwards"""
+    def __init__(self,data):
+        self.data=data
+        self.index =len(data)
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.index ==0:
+            raise StopIteration
+        self.index =self.index - 1
+        return self.data[self.index]
+
+rev =Reverse('spam');
+print( iter(rev) )
+for char in rev:
+    print(char)
