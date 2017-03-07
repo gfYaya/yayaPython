93fc328c1d713602cece9738edd32a69eb9ba523
gfYaya <2645919729@qq.com>
2017-03-07 14:38:53 +0800

    iterator

---
+++ tutorial/brief tour of standard library/error output redirection and program termination.py
@@ -0,0 +1,7 @@
+# coding = utf-8
+
+import sys
+
+# sys.exit()  #this function sys.exit() is a direct way to terminate the
+# scripts,and the following code will not be excuted.
+sys.stderr.write("Warning, log file not found starting a new one\n")
