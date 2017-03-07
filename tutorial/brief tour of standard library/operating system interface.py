471595aba10475eb087748053c2d1dbb1a3ab32b
gfYaya <2645919729@qq.com>
2017-03-07 13:58:25 +0800

    stardard  library

---
+++ tutorial/brief tour of standard library/operating system interface.py
@@ -0,0 +1,11 @@
+# coding=utf-8
+import os
+
+print(os.getcwd())  # Return the current working directory
+os.chdir('E:\\yaya\\yayaPython\\other exercise')  # change current working directory
+os.system('mkdir today')  # run the command mkdir in the system shell
+
+import shutil
+
+shutil.copyfile('', '')
+shutil.move('', '')
