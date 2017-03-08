a44310fe73b48caf1d234522e7a49fb24454f3cf
gfYaya <2645919729@qq.com>
2017-03-08 10:54:25 +0800

    stardard library

---
+++ tutorial/brief tour of standard library/dates and times.py
@@ -0,0 +1,13 @@
+# coding = utf-8
+
+from datetime import date
+
+# dates are easily constructed and formatted
+now = date.today()
+print(now)
+print(now.strftime("%m-%d-%y.%d %b %Y is a %A on the %d day of %B."))
+
+# dates support calendar arithmetic
+birthday = date(1964, 7, 31)
+age = now - birthday
+print(age.days)
