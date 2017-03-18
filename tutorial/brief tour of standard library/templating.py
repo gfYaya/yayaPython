0ae7aa4954b8cf28d113b9d68a227aa1921341c1
gfYaya <2645919729@qq.com>
2017-03-18 13:03:05 +0800

    stardard library

--- tutorial/brief tour of standard library/templating.py
+++ tutorial/brief tour of standard library/templating.py
@@ -2,4 +2,24 @@
 
 from string import Template
 
-t = Template('${village} folk ')
+t = Template('${village} folk send $$10 to $cause')
+print(t.substitute(village='Nottingham', cause='the ditch fund'))
+
+import time, os.path
+
+photofiles = ['img_1074.jpg', 'img_1076.jpg', 'img_1077.jpg']
+
+
+class BatchRename(Template):
+    # class BatchNameByYaya(Template):
+    delimiter = '%'
+
+
+fmt = input('Enter rename style (%d-date %n-seqnum %f-format):  ')
+print(fmt)
+t = BatchRename(fmt)
+date = time.strftime('%d%b%y')
+for i, filename in enumerate(photofiles):
+    base, ext = os.path.splitext(filename)
+    newname = t.substitute(d=date, n=i, f=ext)
+    print('{0}->{1}'.format(filename, newname))
