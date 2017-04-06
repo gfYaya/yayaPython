57f170ad6655c5f85df2ddcf79c7a43416c900c1
gfYaya <2645919729@qq.com>
2017-04-06 22:53:10 +0800

    Core Python Applications Programming , 4  enumerate

--- expert python programming/chapter2 Syntax Best Practices  low/list comprehensions.py
+++ expert python programming/chapter2 Syntax Best Practices  low/list comprehensions.py
@@ -2,4 +2,23 @@
 
 # some exercise about enumerate
 if __name__ == '__main__':
-    pass
+    seq=["one","two","three"]
+
+    i = 0
+    for element in seq:
+        seq[i] = '%d: %s' % (i, seq[i])
+        i += 1
+    print(seq)
+
+    seq = ["one", "two", "three"]
+    for i,element in enumerate(seq):
+        seq[i] = '%d: %s' % (i, seq[i])
+    print(seq)
+
+    def _treatment(pos, element) :
+        return '%d: %s' % (pos, element)
+    seq = ["one", "two", "three"]
+    print([ _treatment(i,seq[i]) for i,seq[i] in enumerate(seq)]) # ['0: three', '1: three', '2: three']
+    # print([_treatment(i,el)  for i,el in enumerate(seq)])
+
+
