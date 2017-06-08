503c2ceee73fa476776d3deb979f276c7a3164bd
gfYaya <2645919729@qq.com>
2017-06-08 20:23:16 +0800

    class exercise

--- expert python programming/Syntax Best Practices Above the Class Level/Subclassing Built-in Types.py
+++ expert python programming/Syntax Best Practices Above the Class Level/Subclassing Built-in Types.py
@@ -8,6 +8,11 @@ class distinctdict(dict):
     def __setitem__(self, key, value):
         try:
             value_index = self.values().index(value)
+            existing_key = self.keys()[value_index]
+            if existing_key != key:
+                raise DistinctError(("This value already exists for '%s'") % str(self[existing_key]))
 
         except ValueError:
             pass
+
+        super()
