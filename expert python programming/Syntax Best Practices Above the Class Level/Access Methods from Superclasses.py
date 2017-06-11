50391368747741090bc6c06adabb1be9817bc4c6
gfYaya <2645919729@qq.com>
2017-06-11 16:13:29 +0800

    class exercise on super

---
+++ expert python programming/Syntax Best Practices Above the Class Level/Access Methods from Superclasses.py
@@ -0,0 +1,27 @@
+# coding = utf-8
+
+class Mama(object):  # this is the old way
+    def says(self):
+        print('do your homework')
+
+
+class Sister(Mama):
+    def says(self):
+        # the funciton is belonged to the superclass Mama.class ,but the argument is pass by itself
+        Mama.says(self)
+        print('and clean your bedroom')
+
+
+anita = Sister()
+anita.says()
+
+
+# another way
+class LittleSister(Mama):
+    def says(self):
+        super(LittleSister, self)
+        print('and clean your bedroom')
+
+
+anita_2 = LittleSister()
+anita_2.says()
