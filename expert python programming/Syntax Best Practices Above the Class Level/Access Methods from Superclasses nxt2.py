febb88fb26f07f5b49ea1f3ef33c4ef90f002862
gfYaya <2645919729@qq.com>
2017-06-13 15:37:56 +0800

    class exercise ,the linearization result is stored in the __mro__ attribute of a class

---
+++ expert python programming/Syntax Best Practices Above the Class Level/Access Methods from Superclasses nxt2.py
@@ -0,0 +1,31 @@
+# coding = utf-8
+
+class BaseBase(object):
+    def method(self):
+        print('BaseBase')
+
+
+class Base1(BaseBase):
+    pass
+
+
+class Base2(BaseBase):
+    def method(self):
+        print('Base2')
+
+
+class MyClass(Base1, Base2):
+    pass
+
+
+here = MyClass()
+here.method()  # Base2
+
+
+def L(klass):
+    # The __mro__ attribute of a class (which is read-only) stores the result of
+    #   the linearization computation, which is done when the class definition is loaded.
+    return [k.__name__ for k in klass.__mro__]
+
+
+print(L(MyClass))  # ['MyClass', 'Base1', 'Base2', 'BaseBase', 'object']
