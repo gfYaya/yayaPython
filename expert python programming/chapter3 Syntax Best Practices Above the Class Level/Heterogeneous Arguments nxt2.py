b3bc4e3ca5ca04d873fb11add02584dbda80db54
gfYaya <2645919729@qq.com>
2017-06-15 17:46:52 +0800

    class inherit exersice on super && __init__

--- expert python programming/chapter3 Syntax Best Practices Above the Class Level/Heterogeneous Arguments nxt2.py
+++ expert python programming/chapter3 Syntax Best Practices Above the Class Level/Heterogeneous Arguments nxt2.py
@@ -1,30 +1,30 @@
 # coding = utf-8
 
 class BaseBase(object):
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         print('basebase')
         super(BaseBase, self).__init__()
 
 
 class Base1(BaseBase):
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         print('base1')
         super(Base1, self).__init__()
 
 
 class Base2(BaseBase):
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         print('base2')
         super(Base2, self).__init__()
 
 
 class MyClass(Base1, Base2):
+    # def __init__(self): #TypeError: __init__() takes 1 positional argument but 2 were given
     def __init__(self, args):
         print("the Mro of MyClass:", MyClass.__mro__)
         print('my base')
-        # super(MyClass, self).__init__()  #TypeError: __init__() missing 1 required positional argument: 'arg'
-        super(MyClass, self).__init__(args)  # TypeError: __init__() takes 1 positional argument but 2 were given
-
+        super(MyClass, self).__init__()
+        # super(MyClass, self).__init__(args)
 
 
 m = MyClass(10)
