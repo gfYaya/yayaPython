2bd361b73e146e4e45ce8e8e1a34fe4d87e4359c
gfYaya <2645919729@qq.com>
2017-07-03 16:58:44 +0800

    __slots__  exercise

---
+++ expert python programming/chapter3 Syntax Best Practices Above the Class Level/Slots.py
@@ -0,0 +1,18 @@
+# coding = utf-8
+
+class Frozen(object):
+    __slots__ = ['ice', 'cream']
+    t = 's'
+
+
+print(dir(Frozen))  # 's','ice','cream' ,they are all included in dir() sequence,does '__slots__' take effect?
+
+print('__dict__' in dir(Frozen))
+print('ice' in dir(Frozen))
+
+# python 声明变量的时候需要同时进行赋值,如同java的局部变量,slots声明这些变量的时候未进行实例化,所以需要使用前先赋值
+glagla = Frozen()
+glagla.ice = 1
+print(glagla.ice)
+glagla.cream = 1
+# glagla.icy = 1
