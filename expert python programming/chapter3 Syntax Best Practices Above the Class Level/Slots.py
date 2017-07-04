e630619e65103235d03e48ae829c9a4d8e01029f
gfYaya <2645919729@qq.com>
2017-07-04 09:40:56 +0800

    __slots__  exercise

--- expert python programming/chapter3 Syntax Best Practices Above the Class Level/Slots.py
+++ expert python programming/chapter3 Syntax Best Practices Above the Class Level/Slots.py
@@ -11,8 +11,10 @@ print('__dict__' in dir(Frozen))
 print('ice' in dir(Frozen))
 
 # python 声明变量的时候需要同时进行赋值,如同java的局部变量,slots声明这些变量的时候未进行实例化,所以需要使用前先赋值
+#  而且slots是用来限制一个实例对象可以拥有那些属性 而不是用来添加属性的 ,但是从glagla.t来看凡是也总有例外
 glagla = Frozen()
 glagla.ice = 1
 print(glagla.ice)
 glagla.cream = 1
-# glagla.icy = 1
+# glagla.icy = 1  # Error
+print(glagla.t)
