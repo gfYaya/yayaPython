8b452f185ac569c6520825b2423bf6a53553c7fd
gfYaya <2645919729@qq.com>
2017-07-04 15:35:28 +0800

    __new__  exercise

--- expert python programming/chapter3 Syntax Best Practices Above the Class Level/The __new__ Method.py
+++ expert python programming/chapter3 Syntax Best Practices Above the Class Level/The __new__ Method.py
@@ -1,6 +1,9 @@
 # coding = utf-8
 
 class MyClass():
+    '''__new__ is the answer to the need for implicit initialization of
+object states. It will let you define an initialization at a lower level than
+__init__, which is always called.'''
     def __new__(cls, *args, **kwargs):
         print('myclass __new__ called')
         return object.__new__(cls)  # default factory
@@ -10,10 +13,22 @@ class MyClass():
         self.a = 1
 
 
-# instance = MyClass()
+instance = MyClass()  # the __new__ method is auto excute before __init__?
+print('---------------------')
 
 class MyOtherClassWithoutAConstructor(MyClass):
     pass
 
 
-instance = MyOtherClassWithoutAConstructor()
+instance = MyOtherClassWithoutAConstructor()  # 自己没有声明init 和 new方法,则直接调用父类的
+
+
+class MyOtherClass(MyClass):
+    def __init__(self):
+        print('MyOhter class __init__ called')
+        super(MyOtherClass, self).__init__()
+        self.b = 2
+
+
+print('------------------')
+instance = MyOtherClass()  # it means that __new__ is called ealier automatically than __init__ method ?
