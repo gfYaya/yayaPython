b6bb68259c0175a11855920ab8b6b8b4fbcec254
gfYaya <2645919729@qq.com>
2017-07-19 19:39:11 +0800

    user defined dict test

---
+++ fluent python/chapter 3 Dictionaries and Sets/the __missing__ Method.py
@@ -0,0 +1,24 @@
+# coding = utf-8
+
+# Example 3-7. StrKeyDict0 converts nonstring keys to str on lookup
+
+class StrKeyDict0(dict):
+    def __missing__(self, key):
+        if isinstance(key, str):
+            raise KeyError(key)
+        return self[str(key)]
+
+    def get(self, key, default=None):
+        try:
+            return self[key]
+        except KeyError:
+            return default
+
+    def __contains__(self, key):
+        return key in self.keys() or str(key) in self.keys()
+
+
+d = StrKeyDict0([('2', 'two'), ('4', 'four')])
+print(d['2'])  # two   => __getitem__ -> __missing__
+print(d.get(2))  # two  => get()
+print('2' in d)  # True  => __contains__
