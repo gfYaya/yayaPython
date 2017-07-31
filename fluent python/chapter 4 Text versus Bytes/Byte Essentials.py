d9a56218fc9600294ffabf9ea443f45ebd19565f
gfYaya <2645919729@qq.com>
2017-07-31 20:22:19 +0800

    byte and bytearray exer

--- fluent python/chapter 4 Text versus Bytes/Byte Essentials.py
+++ fluent python/chapter 4 Text versus Bytes/Byte Essentials.py
@@ -11,3 +11,9 @@ cafe_arr = bytearray(cafe)
 print(cafe_arr)
 print(cafe_arr[0])
 print(cafe_arr[-1:])
+
+import array
+
+numbers = array.array('h', [-2, -1, 0, 1, 2])  # 'h' => short interger
+octets = bytes(numbers)
+print(octets)
