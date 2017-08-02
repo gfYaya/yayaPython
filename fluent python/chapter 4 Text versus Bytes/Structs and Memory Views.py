d33c4782def901f42f0d30195614468765bd9a42
Yaya <2645919729@qq.com>
2017-08-02 23:37:38 +0800

    context exmaple , this demo can't run successfully

---
+++ fluent python/chapter 4 Text versus Bytes/Structs and Memory Views.py
@@ -0,0 +1,5 @@
+# coding = utf-8
+
+# Example 4-4. Using memoryview and struct to inspect a GIF image header
+# It shows the use of memoryview and struct together to extract the width and height of a GIF image.
+
