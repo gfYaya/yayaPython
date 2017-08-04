748461ec63410e60af601c646c3b991e5cd2ba37
gfYaya <2645919729@qq.com>
2017-08-04 11:10:19 +0800

    struct exer

--- fluent python/chapter 4 Text versus Bytes/Structs and Memory Views.py
+++ fluent python/chapter 4 Text versus Bytes/Structs and Memory Views.py
@@ -3,3 +3,16 @@
 # Example 4-4. Using memoryview and struct to inspect a GIF image header
 # It shows the use of memoryview and struct together to extract the width and height of a GIF image.
 
+import struct
+
+fmt = '<3s3sHH'  #
+with open('totoro.gif', 'rb') as fp:
+    img = memoryview(fp.read())
+
+header = img[:10]
+print(bytes(header))
+
+print(struct.unpack(fmt, header))
+
+del header
+del img
