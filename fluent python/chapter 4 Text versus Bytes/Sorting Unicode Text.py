203ae8dd65191bf7e8b02c04c7906320678f12b1
gfYaya <2645919729@qq.com>
2017-08-07 16:11:17 +0800

    locale exer

--- fluent python/chapter 4 Text versus Bytes/Sorting Unicode Text.py
+++ fluent python/chapter 4 Text versus Bytes/Sorting Unicode Text.py
@@ -5,7 +5,9 @@
 import locale  # locale 包依赖的是POXIS协议
 
 # locale.setlocale(locale.LC_COLLATE, 'pt_BR.UTF-8')
-locale.setlocale(locale.LC_COLLATE, 'C.UTF-8')
+# locale.setlocale(locale.LC_COLLATE, 'C.UTF-8')
+# locale.setlocale(locale.LC_COLLATE, 'zh-CN.UTF-8')
+locale.setlocale(locale.LC_ALL, 'english')  # 问题解释:https://www.zhihu.com/question/63537967/answer/210207069
 # 上两个 尤其后者 执行 locale cmd命令,得到的是LC_COLLATE是存在的,值也为C.UTF-8 ,为何还会报错?在Linux上再试试?
 fruits = ['caju', 'atemoia', 'cajá', 'açaí', 'acerola']
 sorted_fruits = sorted(fruits, key=locale.strxfrm)
