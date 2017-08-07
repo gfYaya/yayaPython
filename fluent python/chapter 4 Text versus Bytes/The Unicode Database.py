e2fee180a37cf65cbae4947c655dd751e95524ae
gfYaya <2645919729@qq.com>
2017-08-07 20:10:14 +0800

    PyUCA exer,which is created by James Tauber, prolific Django contributor.

---
+++ fluent python/chapter 4 Text versus Bytes/The Unicode Database.py
@@ -0,0 +1,20 @@
+# coding = utf-8
+
+# Example 4-21. Demo of Unicode database numerical character metadata (callouts describe
+#  each column in the output)
+
+import re
+import unicodedata
+
+re_digit = re.compile(r'\d')
+sample = '1\xbc\xb2\u0969\u136b\u216b\u2466\u2480\u3285'
+
+for char in sample:
+    print('U+%04x' % ord(char),
+          char.center(6),
+          're_dig' if re_digit.match(char) else '-',
+          'isdig' if char.isdigit() else '-',
+          'isnum' if char.isnumeric() else '-',
+          format(unicodedata.numeric(char), '5.2f'),
+          unicodedata.name(char),
+          sep='\t')
