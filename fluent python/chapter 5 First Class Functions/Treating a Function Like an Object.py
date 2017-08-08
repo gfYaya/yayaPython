291e421295781680eccdeeca0dd26101b4a86708
gfYaya <2645919729@qq.com>
2017-08-08 10:27:10 +0800

    function test ,and the questions in the map operation is fixed.

--- fluent python/chapter 5 First Class Functions/Treating a Function Like an Object.py
+++ fluent python/chapter 5 First Class Functions/Treating a Function Like an Object.py
@@ -4,7 +4,7 @@
 
 def factorial(n):
     '''returns n1'''
-    '''ssss'''  # is not print in the console as the interpreter run the 'print(factorial.__doc__)' operation
+    '''ssss'''  # It's not printed in the console as the interpreter run the 'print(factorial.__doc__)' operation
     # return 1 if n < 2 else n * factorial(n - 1)  #=>1405006117752879898543142606244511569936384000000000
     return 1 if n == 1 else n * factorial(n - 1)  # =>1405006117752879898543142606244511569936384000000000
     # return 1 if 1 else n * factorial(n - 1)  # => 1
@@ -25,8 +25,10 @@ iterable), range(10) in this example."""
 
 fact = factorial
 print(fact)
-print(fact(5))
+# print(fact(5))
 map_a = map(factorial, range(11))
 print(map_a)
-# print(list(map_a)) # recursion  => RecursionError: maximum recursion depth exceeded in comparison
-# print(list(map(fact,range(11)))) => RecursionError: maximum recursion depth exceeded in comparison
+print(list(map_a))  # factorial returns 1 if n == 1 else n * factorial(n - 1) ,it gets bitten by the
+# bug ErrorRecursion,because the first value is zero,then the function pass a negative argu in
+# the next step with Tail Recursion
+# print(list(map(fact,range(11))))
