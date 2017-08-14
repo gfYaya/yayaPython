cf793aed118593f7548da216e75c5a0e431cb196
gfYaya <2645919729@qq.com>
2017-08-14 08:55:24 +0800

    function test

---
+++ fluent python/chapter 6 Design Patterns with First-Class Functions/Classic Strategy.py
@@ -0,0 +1,74 @@
+# coding = utf-8
+
+# Example 6-1. Implementation Order class with pluggable discount strategies
+
+from abc import ABC, abstractclassmethod
+from collections import namedtuple
+
+Customer = namedtuple('Customer', 'name fidelity')
+
+
+class LineItem:
+    def __init__(self, product, quantity, price):
+        self.product = product
+        self.quantity = quantity
+        self.price = price
+
+    def total(self) -> 'int':
+        return self.quantity * self.price
+
+
+class Order:  # the context
+
+    def __init__(self, customer, cart: '购物篮', promotion=None):
+        self.customer = customer
+        self.cart = cart
+        self.promotion = promotion
+
+    def total(self):
+        if not hasattr(self, '__total'):
+            self.__total = sum(item.total() for item in self.cart)
+        return self.__total
+
+    def due(self):
+        if self.promotion is None:
+            discount = 0
+        else:
+            discount = self.promotion.discount(self)
+        return self.total() - discount
+
+    def __repr__(self):
+        fmt = '<Order total: {:.2f} due: {:.2f}>'
+        return fmt.format(self.total(), self.due())
+
+
+class Promotion(ABC):  # the Strategy: an abstract base class
+
+    @abstractclassmethod
+    def discount(self, order):
+        """Return discount as a positive dollar amount"""
+
+
+class FidelityPromo(Promotion):  # first Concrete Strategy
+    """5% discount for customers with 1000 or more fidelity points"""
+
+    def discount(self, order):
+        return order.total() * .05 if order.customer.fidelity >= 1000 else 0
+
+
+class BulkItemPromo(Promotion):  # second Concrete Strategy
+    """10% discount for each LineItem with 20 or more units"""
+
+    def discount(self, order):
+        discount = 0
+        for item in order.cart:
+            if item.quantity >= 20:
+                discount += item.total() * .1
+
+        return discount
+
+
+class LargeOrderPromo(Promotion):  # third Concrete Strategy
+    """7% discount for orders with 10 or more distinct items"""
+
+    def discount(self, order):
