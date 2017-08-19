dd07dea99497eb2767e766726904af3b7ae78f7f
gfYaya <2645919729@qq.com>
2017-08-19 17:17:38 +0800

    decorator exercise

--- fluent python/chapter 7 Function Decorators and Closures/How It Works.py
+++ fluent python/chapter 7 Function Decorators and Closures/How It Works.py
@@ -24,3 +24,7 @@ def clock(func):
         return result
 
     return clocked
+
+# 已经证明 如果装饰器如此定义,那么必须要有 decorator()这种立即执行,装饰器函数名称后面必须要有括号,传入的第一个参数认为是
+# 装饰器的参数 ,如果不加入圆括号,则传入的第一个参数 是被装饰器修饰的函数对象
+# expert python grogramming下chapter2 中 decorators.py的代码
