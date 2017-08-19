37ce0a1c160b1e6171a2dbeff1b5322d0e8a3408
gfYaya <2645919729@qq.com>
2017-08-19 15:46:55 +0800

    decorator exercise

---
+++ fluent python/chapter 7 Function Decorators and Closures/Implementing a Simple Decorator.py
@@ -0,0 +1,42 @@
+# coding = utf-8
+
+# Example 7-15. A simple decorator to output the running time of functions
+
+import time
+
+
+def clock(func):
+    print('decorated clock starts to load')
+
+    def clocked(*args):
+        t0 = time.perf_counter()
+        result = func(*args)
+        elapse = time.perf_counter() - t0
+        name = func.__name__
+        # arg_str = ', '.join(repr(arg) for arg in args)
+        arg_str = ', '.join(str(arg) for arg in args)
+        print('[%0.8fs] %s(%s) -> %r' % (elapse, name, arg_str, result))
+        return result
+
+    return clocked
+
+
+# Example 7-16. Using the clock decorator
+
+# clockdeco_demo.py
+
+@clock
+def snooze(seconds: float) -> None:
+    time.sleep(seconds)
+
+
+@clock
+def factorial(n: int):
+    return 1 if n < 2 else n * factorial(n - 1)
+
+
+if __name__ == '__main__':
+    print('*' * 40, 'Calling snooze(.123)')
+    snooze(.123)
+    print('*' * 40, 'Calling facorial(6)')
+    print('6! = ', factorial(6))
